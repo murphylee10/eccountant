690a849056c4f3193545a832acc566991113e579
Murphy Lee <murphylee2004@gmail.com>
2024-06-23 14:13:06 -0400

    fix: nodemon for windows

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express, { Express, Request, Response } from "express";
+import { transactionsRouter } from "./routers/transactions_router";
+import bodyParser from "body-parser";
 import "dotenv/config";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/transactions", transactionsRouter);
 
 app.listen(PORT, () => {
   console.log("HTTP server on http://localhost:%s", PORT);
