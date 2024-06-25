f8ee37c9814e70079f3b048efe1f719b10cf9640
murphylee10 <murphylee2004@gmail.com>
2024-06-25 00:42:27 -0400

    feat: error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from "express";
-import { transactionsRouter } from "./routers/transactions_router";
+import { transactionsRouter } from "./routers/transactions";
 import bodyParser from "body-parser";
 import "dotenv/config";
+import { errorHandler } from "./middleware/errors";
+import "express-async-errors"
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/transactions", transactionsRouter);
+
+// Error handling
+app.use(errorHandler); 
 
 app.listen(PORT, () => {
   console.log("HTTP server on http://localhost:%s", PORT);
