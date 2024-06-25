e41c856e201ad2e7e311b1bf4f8b25d964305a25
Murphy Lee <murphylee2004@gmail.com>
2024-06-25 01:31:37 -0400

    feat: prisma setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,8 @@ import { transactionsRouter } from "./routers/transactions";
 import bodyParser from "body-parser";
 import "dotenv/config";
 import { errorHandler } from "./middleware/errors";
-import "express-async-errors"
+import "express-async-errors";
+import prisma from "@/utils/prisma/client";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -14,8 +15,19 @@ app.use(bodyParser.json());
 app.use("/api/transactions", transactionsRouter);
 
 // Error handling
-app.use(errorHandler); 
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("HTTP server on http://localhost:%s", PORT);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
