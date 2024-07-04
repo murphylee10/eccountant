1c63583a640c4c556f21741b24fdd334887cfbfb
Chad Rossouw <chadrossouw7247@gmail.com>
2024-07-04 14:46:52 -0400

    feat(web): added links and moved pages

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,24 +1,24 @@
-import { Routes } from '@angular/router';
-import { LayoutComponent } from './pages/layout/layout.component';
-import { TransactionsComponent } from './pages/transactions/transactions.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LinksComponent } from '@pages/links/links.component';
-import { authGuard } from './utils/auth.guard';
+import { Routes } from "@angular/router";
+import { LayoutComponent } from "./pages/layout/layout.component";
+import { TransactionsComponent } from "./pages/transactions/transactions.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { LinksComponent } from "@pages/links/links.component";
+import { authGuard } from "./utils/auth.guard";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      { path: '', component: HomeComponent },
-      { path: 'accounts', component: LinksComponent, canActivate: [authGuard] },
-      {
-        path: 'transactions',
-        component: TransactionsComponent,
-        canActivate: [authGuard],
-      },
-    ],
-  },
-  { path: '**', component: HomeComponent },
+	{
+		path: "",
+		component: LayoutComponent,
+		children: [
+			{ path: "demo", component: HomeComponent },
+			{ path: "accounts", component: LinksComponent, canActivate: [authGuard] },
+			{
+				path: "transactions",
+				component: TransactionsComponent,
+				canActivate: [authGuard],
+			},
+			{ path: "**", redirectTo: "transactions" },
+		],
+	},
+	{ path: "**", redirectTo: "transactions" },
 ];
