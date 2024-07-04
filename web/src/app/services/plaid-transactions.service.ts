5b59a9f34d57b56bc6bbec18a7f304e704f9f4d7
Murphy Lee <murphylee2004@gmail.com>
2024-07-03 23:36:46 -0400

    fix: plaid transaction type

--- web/src/app/services/plaid-transactions.service.ts
+++ web/src/app/services/plaid-transactions.service.ts
@@ -1,18 +1,19 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import environment from "@environment";
-import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import environment from '@environment';
+import { Observable } from 'rxjs';
+import { PlaidTransaction } from '../models/transaction.model';
 
 @Injectable({
-	providedIn: "root",
+  providedIn: 'root',
 })
 export class PlaidTransactionsService {
-	private transactionsUrl = "/transactions";
-	constructor(private http: HttpClient) {}
+  private transactionsUrl = '/transactions';
+  constructor(private http: HttpClient) {}
 
-	getTransactions(): Observable<any> {
-		return this.http.get<any>(
-			encodeURI(`${environment.api_url}${this.transactionsUrl}/`),
-		);
-	}
+  getTransactions(): Observable<PlaidTransaction[]> {
+    return this.http.get<PlaidTransaction[]>(
+      encodeURI(`${environment.api_url}${this.transactionsUrl}/`),
+    );
+  }
 }
