430d06475527b723da11100ca327f925bf7b19ea
Chad Rossouw <chadrossouw7247@gmail.com>
2024-07-23 21:35:34 -0400

    feat(all): added support for multiple user devices to realtime events

--- backend/src/utils/events/event-dispatcher.ts
+++ backend/src/utils/events/event-dispatcher.ts
@@ -8,7 +8,7 @@ const EVENTBUS_ENDPOINT = "/eventbus";
 
 export default class EventDispatcher {
   private static instance: EventDispatcher;
-  private wsMap: Map<string, WebSocket> = new Map();
+  private wsMap: Map<string, WebSocket[]> = new Map();
 
   private constructor(app: Application) {
     this.setupWs(app);
@@ -32,18 +32,34 @@ export default class EventDispatcher {
       });
       ws.on("close", () => {
         console.log("The connection was closed!");
+        // Get array of currently connected websockets for user.
+        const arr = this.wsMap.get(sub);
+        if (!arr) return;
+
+        // Remove this websocket from array.
+        const idx = arr.indexOf(ws);
+        arr.splice(idx);
+
+        // If no websockets left for user, remove user from map.
+        if (arr.length == 0) this.wsMap.delete(sub);
       });
 
-      this.wsMap.set(sub, ws);
+      // If user already in map, add this websocket to user's websocket array.
+      if (this.wsMap.has(sub)) this.wsMap.get(sub)?.push(ws);
+      // Otherwise, add user to map and attach this websocket.
+      else this.wsMap.set(sub, [ws]);
     });
   }
 
   public notifyUser(event: Event<any>, sub: string) {
     if (!this.wsMap.has(sub)) return;
-    this.wsMap.get(sub)?.send(event.serialize());
+    // For every websocket owned by user, send event.
+    for (const ws of this.wsMap.get(sub)!.values()) ws.send(event.serialize());
   }
 
   public notifyAll(event: Event<any>) {
-    for (const ws of this.wsMap.values()) ws.send(event.serialize());
+    // For every user in map, for every websocket owned by user, send event.
+    for (const userSockets of this.wsMap.values())
+      for (const ws of userSockets.values()) ws.send(event.serialize());
   }
 }
