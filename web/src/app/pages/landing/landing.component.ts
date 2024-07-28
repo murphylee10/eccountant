56f9b4a3c3e16e056060a99672f576f3ffd6c031
Murphy Lee <murphylee2004@gmail.com>
2024-07-28 02:21:45 -0400

    feat: landing start

--- web/src/app/pages/landing/landing.component.ts
+++ web/src/app/pages/landing/landing.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, type OnInit } from "@angular/core";
+// biome-ignore lint/style/useImportType: angular needs the whole module imported
+import { Router } from "@angular/router";
+// biome-ignore lint/style/useImportType: angular needs the whole module imported
+import { AuthService } from "@auth0/auth0-angular";
+import { ButtonModule } from "primeng/button";
+import { MenubarModule } from "primeng/menubar";
 
 @Component({
-  selector: 'app-landing',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing.component.html',
-  styles: '',
+	selector: "app-landing",
+	standalone: true,
+	imports: [MenubarModule, ButtonModule, CommonModule],
+	templateUrl: "./landing.component.html",
+	styles: "",
 })
-export class LandingComponent {}
+export class LandingComponent implements OnInit {
+	isAuthenticated = false;
+
+	constructor(
+		private auth: AuthService,
+		private router: Router,
+	) {}
+
+	ngOnInit(): void {
+		this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+			this.isAuthenticated = isAuthenticated;
+		});
+	}
+
+	handleButtonClick(): void {
+		if (this.isAuthenticated) {
+			this.router.navigate(["/user/dashboard"]);
+		} else {
+			this.router.navigate(["/sign-in"]);
+		}
+	}
+
+	logout(): void {
+		this.auth.logout();
+	}
+}
